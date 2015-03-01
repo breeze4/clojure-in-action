1e9f4a8122747e5762601eed073a6f5d073462e9
breeze4 <ambiturner0@gmail.com>
2015-02-28 19:52:40 -0800

    added more ORM functionality

--- cia/src/cia/chapter09/user.clj
+++ cia/src/cia/chapter09/user.clj
@@ -15,31 +15,61 @@
 (defentity charges
            (belongs-to users))
 
+(has-many users charges)
+
 (defn add-user [u] (insert users (values u)))
 (defn add-charge [c] (insert charges (values c)))
 
+(defn validate-user [user]
+                    "Returns the valid user or nothing."
+                    (if-let [email (:email_address user)]
+                      user))
+
+(defn create-user [user]
+  (-> user
+      validate-user
+      add-user))
+
+(defn validate-charge [charge]
+  "Returns the charge if it is a positive amount."
+  (if-let [total-amount (+ (* 100 (:amount_dollars charge))
+                           (:amount_cents charge))]
+    (if (> total-amount 0)
+      charge)))
+
+(defn create-charge [charge]
+  (-> charge
+      validate-charge
+      add-charge))
+
 ; add some sample data to exercise it
-(add-user {:login         "rob"
-           :first_name    "Robert"
-           :last_name     "Berger"
-           :password      "secret"
-           :email_address "rob@runa.com"})
+(create-user {:login         "rob"
+         :first_name    "Robert"
+         :last_name     "Berger"
+         :password      "secret"
+         :email_address "rob@runa.com"})
 
-(add-user {:login         "rob2"
-           :first_name    "Robert"
-           :last_name     "Stevenson"
-           :password      "friday"
-           :email_address "rob@crusoe.com"})
+(create-user {:login         "rob2"
+         :first_name    "Robert"
+         :last_name     "Stevenson"
+         :password      "friday"
+         :email_address "rob@crusoe.com"})
 
-(add-charge {:user_id        1
+(create-charge {:user_id        1
              :amount_dollars 50
              :amount_cents   23
              :category       "financial"
              :vendor_name    "vanguard"})
 
+(create-charge {:user_id        1
+             :amount_dollars 34
+             :amount_cents   15
+             :category       "ice cream"
+             :vendor_name    "the soda shoppe"})
+
 (select users)
 ;(select users (where {:id 2}))
-
+(select charges (where {:user_id 1}))
 ;(update users
 ;        (set-fields {:login "stevenson" :id 5})
 ;        (where {:id 2}))
